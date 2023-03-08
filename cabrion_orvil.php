fee9a6ee9a6402bdff3047a896bba31837ad936b
orvilcab <126080124+orvilcab@users.noreply.github.com>
2023-03-08 15:49:33 +0100

    formulaire
    fonctionnel

--- cabrion_orvil.php
+++ cabrion_orvil.php
@@ -1,21 +1,21 @@
 <?php
 
-	$Civilite = htmlspecialchars($_POST["Civilite"]);
-	$Nom = htmlspecialchars($_POST["Nom"]);
-	$Prenom = htmlspecialchars($_POST["Prenom"]);
-	$Adresse = htmlspecialchars($_POST["Adresse"]);
-	$Remarques = htmlspecialchars($_POST["Remarques"]);
+	$civilite = htmlspecialchars($_POST["civilite"]);
+	$nom = htmlspecialchars($_POST["nom"]);
+	$prenom = htmlspecialchars($_POST["prenom"]);
+	$mail = htmlspecialchars($_POST["mail"]);
+	$tel = htmlspecialchars($_POST["tel"]);
 
-	echo 'Merci ' . $Nom . ' !';
+	echo 'Merci ' . $civilite .' ' . $nom . ' !';
 
 	// 1 : on ouvre le fichier
-	$monfichier = fopen('donneesformulaire.txt', 'a+');
+	$monfichier = fopen('Cabrion_orvil.txt', 'a+');
 	fputs($monfichier, "==================================\n"); 
-	fputs($monfichier, $Civilite."\n");
-	fputs($monfichier, $Nom."\n"); 
-	fputs($monfichier, $Prenom."\n"); 
-	fputs($monfichier, $Adresse."\n"); 
-	fputs($monfichier, $Remarques."\n"); 
+	fputs($monfichier, $civilite."\n");
+	fputs($monfichier, $nom."\n"); 
+	fputs($monfichier, $prenom."\n"); 
+	fputs($monfichier, $mail."\n"); 
+	fputs($monfichier, $tel."\n"); 
 
 	// 3 : quand on a fini de l'utiliser, on ferme le fichier
 	fclose($monfichier);
