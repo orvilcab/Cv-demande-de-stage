2f39353b7444c018b59a805b5395722f9041c0dc
orvilcab <126080124+orvilcab@users.noreply.github.com>
2023-03-09 16:43:51 +0100

    commit
    16:43 9/03/2023

--- cabrion_orvil.php
+++ cabrion_orvil.php
@@ -1,21 +1,25 @@
 <?php
 
 	$civilite = htmlspecialchars($_POST["civilite"]);
+	$societe = htmlspecialchars($_POST["societe"]);
 	$nom = htmlspecialchars($_POST["nom"]);
 	$prenom = htmlspecialchars($_POST["prenom"]);
 	$mail = htmlspecialchars($_POST["mail"]);
 	$tel = htmlspecialchars($_POST["tel"]);
+	$commentaire = htmlspecialchars($_POST["commentaire"]);
 
 	echo 'Merci ' . $civilite .' ' . $nom . ' !';
 
 	// 1 : on ouvre le fichier
-	$monfichier = fopen('Cabrion_orvil.txt', 'a+');
+	$monfichier = fopen('cabrion_orvil.txt', 'a+');
 	fputs($monfichier, "==================================\n"); 
 	fputs($monfichier, $civilite."\n");
+	fputs($monfichier, $societe."\n");
 	fputs($monfichier, $nom."\n"); 
 	fputs($monfichier, $prenom."\n"); 
 	fputs($monfichier, $mail."\n"); 
-	fputs($monfichier, $tel."\n"); 
+	fputs($monfichier, $tel."\n");
+	fputs($monfichier, $commentaire."\n"); 
 
 	// 3 : quand on a fini de l'utiliser, on ferme le fichier
 	fclose($monfichier);
